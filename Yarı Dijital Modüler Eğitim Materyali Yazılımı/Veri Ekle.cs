37654da47100bcc96bc174311b8d578aa9610945
Yahya Tezcan <yahya.tezcan@hotmail.com>
2022-05-08 20:38:54 +0300

    Proje dosyası ekle.

---
+++ Yarı Dijital Modüler Eğitim Materyali Yazılımı/Veri Ekle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yarı_Dijital_Modüler_Eğitim_Materyali_Yazılımı
+{
+    public partial class Veri_Ekle : Form
+    {
+        public Veri_Ekle()
+        {
+            InitializeComponent();
+        }
+
+        private void Veri_Ekle_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
